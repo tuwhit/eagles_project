609be98f1edfe099ee2a4b7d550af53c0a7e6d59
Hyunkyung Ahn <tuwhit1202@gmail.com>
2019-01-30 14:20:37 +0900

    Request 검증 코드 외부로 뺌

--- slackbot/views.py
+++ slackbot/views.py
@@ -1,12 +1,15 @@
+import hmac
+
 from django.shortcuts import render
 
 # Create your views here.
 # eagle 오늘, 어제 경기 정보
 from rest_framework.views import APIView
-from slackbot import games, verify_requests
+from slackbot import games
 from datetime import date, timedelta
 from rest_framework.response import Response
 from rest_framework import status
+import time
 import requests
 import json
 
@@ -58,26 +61,37 @@ class kbo(APIView):
       ]
     }
 
-    if verify_requests(request.META['X-Slack-Request-Timestamp'], request.META['X-Slack-Signature'], request.data):
-      try:
-        if 'text' in request.data and request.data['text']:
-          score_str = games.get_all_games(request.data['text'])
-        else:
-          # 오늘, 어제 경기 스코어 가져옴
-          today = date.today()
-          yesterday = today - timedelta(1)
+    slack_signing_secret = '8c91f513bae502d0ed124a2d23c05cf2'
+    ts = request.META['X-Slack-Request-Timestamp']
 
-          score_str = games.get_all_games(str(yesterday)) + '\n' +  games.get_all_games(str(today))
-
-        response['attachments'][0]['text'] = score_str
-
-        return Response(response, status=status.HTTP_200_OK)
-      except:
-        response['attachments'][0]['text'] = '스코어를 불러오는데 문제가 발생했습니다.'
-
-        return Response(response, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    else:
+    if time.time() - ts > 60 * 5:
       return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+    sig_basestring = 'v0:' + ts + ':' + request.data
+    my_signature = 'v0=' + hmac.compute_hash_sha256(slack_signing_secret, sig_basestring).hexdigest()
+
+    slack_signature = request.META['X-Slack-Signature']
+
+    if not hmac.compare(my_signature, slack_signature):
+      return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+    try:
+      if 'text' in request.data and request.data['text']:
+        score_str = games.get_all_games(request.data['text'])
+      else:
+        # 오늘, 어제 경기 스코어 가져옴
+        today = date.today()
+        yesterday = today - timedelta(1)
+
+        score_str = games.get_all_games(str(yesterday)) + '\n' +  games.get_all_games(str(today))
+
+      response['attachments'][0]['text'] = score_str
+
+      return Response(response, status=status.HTTP_200_OK)
+    except:
+      response['attachments'][0]['text'] = '스코어를 불러오는데 문제가 발생했습니다.'
+
+      return Response(response, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class auth(APIView):
